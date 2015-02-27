b724fc6f6233bfb78e466d9d68c509001b6cce8e
christabor <dxdstudio@gmail.com>
2015-02-26 20:15:13 -0800

    Refactor graph ADT:
    -Remove unnecessary in/out keys and dict for directed graph
    -Add some new graph methods
    -Fix some broken methods
    -Add some basic assertions

--- data_structures/graphs/graphs.py
+++ data_structures/graphs/graphs.py
@@ -11,6 +11,8 @@ from pprint import pprint as ppr
 from random import randrange
 from random import choice
 
+# wikipedia.org/wiki/Glossary_of_graph_theory
+
 MAX_VERTICES = 6
 MAX_EDGES = MAX_VERTICES / 2
 all_vertices = range(0, MAX_VERTICES)
@@ -23,123 +25,113 @@ class InvalidGraphRepresentation(Exception):
 class Graph:
 
     def __len__(self):
-        return len(self.nodes.keys())
+        return len(self.vertices.keys())
 
-    def __init__(self, nodes=None):
-        self.nodes = nodes or {}
+    def __init__(self, vertices=None):
+        self.vertices = vertices or {}
 
     def __setitem__(self, node, edges):
-        self.nodes[node] = edges
+        self.vertices[node] = edges
 
     def __delitem__(self, node):
-        del self.nodes[node]
+        del self.vertices[node]
 
     def __getitem__(self, node):
-        return self.nodes[node]
-
-    def tour(self, start, end, path=[]):
-        path, paths = path + [start], []
-        if start == end:
-            return path
-        if start in self.nodes:
-            # {'start': [], '...': [], '...': []}
-            for node in self.nodes[start]:
-                # {'start': ['...', '...', '...']}
-                # Add new node if it's not in the list already.
-                if node not in path:
-                    # Get new path from this node
-                    sub_paths = self.tour(node, end, path=path)
-                    for sub_path in sub_paths:
-                        paths.append(sub_path)
-        return paths
+        return self.vertices[node]
 
     def __iter__(self):
-        return iter(self.nodes)
+        return iter(self.vertices)
 
     def __str__(self):
         display = []
-        for vertex, nodes in self.nodes.iteritems():
-            sub_nodes = ''
-            for node in nodes:
-                sub_nodes += '--({})'.format(node)
-            display.append('({}){}'.format(vertex, sub_nodes))
+        for vertex, vertices in self.vertices.iteritems():
+            display.append('{outbound} <-- ({vertex})'.format(
+                vertex=vertex,
+                outbound=vertices))
         return ',\n'.join(display)
+
+    def walk(self, start, end, path=[]):
+        path = path + [start]
+        paths = []
+        if start == end:
+            return path
+        if start in self.vertices:
+            for node in self.vertices[start]:
+                # Add new node if it's not in the list already.
+                if node not in path:
+                    # Get new path from this node
+                    paths += self.walk(node, end, path=path)
+        return paths
 
     def connections(self, node):
         try:
-            return '({})--{}'.format(node, self.nodes[node])
+            return '({})--{}'.format(node, self.vertices[node])
         except KeyError:
             raise InvalidGraphRepresentation
 
-    def get_vertices(self):
-        vertices = [] + self.nodes.keys()
-        for vals in self.nodes.values():
+    def all_vertices(self):
+        vertices = self.vertices.keys()
+        for vals in self.vertices.values():
             vertices = vertices + vals
-        return vertices
+        return list(set(vertices))
 
-    def get_degree(self, vertex):
-        slots = {}
-        nodes = self.get_vertices()
-        for node in nodes:
-            try:
-                slots[node] += 1
-            except KeyError:
-                slots[node] = 0
-        return slots[vertex]
+    def degree(self, node):
+        return len(self.vertices[node]) if node in self.vertices else 0
 
-    def has_degree(self, degrees, vertex=None):
-        try:
-            return self.get_degree(vertex) == degrees
-        except KeyError:
-            return False
+    def has_degree(self, node, degrees):
+        return self.degree(node) == degrees
 
-    def has_multiple_degrees(self):
-        nodes = self.get_vertices()
-        unique = list(set(nodes))
-        if len(self.nodes) != len(unique):
-            return False
-        return True
+    def has_multiple_degrees(self, node):
+        return self.degree(node) > 1
 
-    def hamiltonian(self):
+    def is_open(self, start, end):
+        return not self.is_closed(start, end)
+
+    def is_closed(self, start, end):
+        """A walk is considered closed if the first and last
+        vertices are the same, open if not."""
+        if start == end:
+            return True
+        res = self.walk(start, end)
+        return res[0] == res[::-1]
+
+    def is_trail(self, start, end):
+        """A trail is a walk in which all the edges are distinct."""
+        res = self.walk(start, end)
+        return len(res) == len(set(res))
+
+    def tour_hamiltonian(self):
         pass
 
-    def eulerian(self):
+    def tour_eulerian(self):
         pass
 
 
 class DirectedGraph(Graph):
-
-    def __setitem__(self, *args):
-        node, directions = args
-        # Use a set to prevent duplicated in/outgoing edges.
-        self.nodes[node] = directions
-
-    def __delitem__(self, node):
-        del self.nodes[node]
-
-    def __str__(self):
-        display = []
-        for vertex, nodes in self.nodes.iteritems():
-            display.append('{outbound} <-- ({vertex}) <-- {inbound}'.format(
-                vertex=vertex,
-                inbound=list(nodes['in']),
-                outbound=list(nodes['out'])))
-        return ',\n'.join(display)
+    pass
 
 
 def _rand_edges(num_edges):
     global all_vertices
-    return [choice(all_vertices) for _ in range(num_edges)]
+
+    def _graph(num_edges):
+        return [choice(all_vertices) for _ in range(num_edges)]
+    edges = set(_graph(num_edges))
+
+    # Since it's random, force unique graph
+    while len(edges) < num_edges:
+        edges = set(_graph(num_edges))
+    return list(edges)
 
 
-def _test_tour_valid(graph):
+def _test_walk_valid(graph):
     valid = False
     while not valid:
         start = choice(all_vertices)
         end = choice(all_vertices)
-        res = graph.tour(start, end)
+        res = graph.walk(start, end)
         if len(res) > 0:
-            return '{}, {}: Tour results: {}'.format(start, end, res)
+            return 'walk of {}, {} = {}'.format(start, end, res)
             valid = True
 
 if __name__ == '__main__':
@@ -149,12 +141,23 @@ if __name__ == '__main__':
         for _ in range(5):
             graph[choice(all_vertices)] = _rand_edges(MAX_EDGES)
 
-        _print('Generated graph', graph.nodes, func=ppr)
-        _print('Tour of...', _test_tour_valid(graph))
+        test_graph = Graph({
+            0: [1, 2],
+            1: [2, 0],
+            2: [0]
+        })
+        print(test_graph.walk(1, 2))
+        assert len(test_graph.walk(0, 3)) == 0  # non-existent node
+        assert len(test_graph.walk(2, 1)) == 3  # directions are enforced
+        assert not test_graph.is_closed(1, 2)
+        assert test_graph.is_closed(0, 0)
 
-        deg, ver = randrange(0, MAX_EDGES), choice(all_vertices)
+        _print('Generated graph', graph.vertices, func=ppr)
+        _print('walk of...', _test_walk_valid(graph))
+
+        deg, vertex = randrange(0, MAX_EDGES), choice(all_vertices)
         print('Has degree {} ... {}? {}'.format(
-            deg, ver, graph.has_degree(deg, vertex=ver)))
+            deg, vertex, graph.has_degree(deg, vertex)))
 
         for _ in range(5):
             try:
@@ -163,22 +166,26 @@ if __name__ == '__main__':
                 print(
                     'Invalid graph was generated -- need more than 2 vertices')
         for node in graph:
-            print('Node {} has degree {}'.format(node, graph.get_degree(node)))
+            print('Node {} has degree {}'.format(node, graph.degree(node)))
         _print('graph', graph)
-        _print('Has multiple degrees?', graph.has_multiple_degrees())
+        _print('Has multiple degrees?', graph.has_multiple_degrees(1))
 
     with Section('Directed Graph'):
         digraph = DirectedGraph()
         for n in range(MAX_VERTICES):
-            digraph[n] = {
-                'in': [_rand_edges(MAX_EDGES)],
-                'out': [_rand_edges(MAX_EDGES)]
-            }
-        _print('Generated directed-graph', digraph.nodes, func=ppr)
+            digraph[n] = _rand_edges(MAX_EDGES)
+        _print('Generated directed-graph', digraph.vertices, func=ppr)
         _print('Digraph', digraph)
         _print('Get item:', digraph[4])
-        digraph[3] = {'in': [1, 2, 4], 'out': [3, 2, 5]}
+        digraph[3] = [3, 2, 5]
         _print('Set item:', digraph[3])
         del digraph[2]
         _print('Del item:', digraph)
-        _print('Tour of...', _test_tour_valid(digraph))
+        _print('walk of...', _test_walk_valid(digraph))
+
+        test_digraph = DirectedGraph({
+            0: [1, 2],
+            1: [2, 0],
+            2: [0]
+        })
+        print(test_digraph)
