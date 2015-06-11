e0e4221ad10d59a97a2f130cb34626c3d175d5aa
christabor <dxdstudio@gmail.com>
2015-06-10 21:12:29 -0700

    Update datamaker helpers

--- helpers/datamaker.py
+++ helpers/datamaker.py
@@ -3,14 +3,17 @@ from random import choice
 from string import ascii_uppercase
 
 
-def random_dag():
-    max = 10
-    letters = [choice(ascii_uppercase) for _ in range(max)]
+def random_node(vals, nums, max_edges):
+    return {
+        'edges': [choice(nums) for _ in range(rr(0, max_edges))],
+        'val': choice(vals)
+    }
+
+
+def random_graph(max_edges=10):
+    letters = [choice(ascii_uppercase) for _ in range(max_edges)]
     dag = {}
-    nums = range(max)
+    nums = range(max_edges)
     for k, letter in enumerate(letters):
-        dag[k] = {
-            'edges': [choice(nums) for _ in range(rr(0, max))],
-            'val': choice(letter)
-        }
+        dag[k] = random_node(letter, nums, max_edges)
     return dag
