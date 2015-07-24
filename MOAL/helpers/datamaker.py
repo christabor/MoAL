191f49f4d478916188a339f8e2be49d69a623bed
christabor <dxdstudio@gmail.com>
2015-07-23 22:15:04 -0700

    Add matrix multiplication examples to exponent function, docstrings and helper.

--- MOAL/helpers/datamaker.py
+++ MOAL/helpers/datamaker.py
@@ -28,8 +28,37 @@ def subnet_mask():
 
 
 def random_dna(max=4):
+    """Generate a random string of DNA.
+
+    Kwargs:
+        max (int) - The maximum length of the string.
+
+    >>> random_dna(max=4)
+    'TCGA'
+    """
     dna = ''
     acids = ['T', 'G', 'A', 'C']
     for x in range(max):
         dna += choice(acids)
     return dna
+
+
+def random_matrix(rows=4, columns=4, min=1, max=100):
+    """Generate a matrix filled with random numbers.
+
+    Kwargs:
+        rows (int) - The number of rows.
+        columns (int) - The number of columns for each row.
+        min (int) - The minimum number to use in randrange
+        max (int) - The maximum number to use in randrange
+
+    >>> random_matrix(rows=3, columns=3, min=1, max=2)
+    [[94, 23, 50], [57, 28, 52], [94, 5, 45]]
+    """
+    matrix = []
+    for row in range(rows):
+        _row = []
+        for _ in range(columns):
+            _row.append(rr(min, max))
+        matrix.append(_row)
+    return matrix
